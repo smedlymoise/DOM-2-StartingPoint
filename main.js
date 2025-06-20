bb24fceab59bba128dde8b54ecdaa500de679e23
Smedly Moise <smedly.moise@gmail.com>
2025-06-20 14:38:05 -0400

    1st upoload to feature branch

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-// Please feel free to change the JS as you see fit! This is just a starting point.
+
 
 const root = document.getElementById("root");
 root.addEventListener("click", (event) => {
   console.log(event.target.tagName);
   console.log(event.target);
 });
+
+
