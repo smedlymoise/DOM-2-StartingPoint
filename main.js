b38f116589d7f6f6f7510e157893ad280e525638
Darrell Moreno <dmoreno1987@outlook.com>
2025-06-20 14:47:36 -0400

    Added more colors in the HTML dropdown through javascript code

--- main.js
+++ main.js
@@ -5,3 +5,14 @@ root.addEventListener("click", (event) => {
   console.log(event.target.tagName);
   console.log(event.target);
 });
+
+const colorSelect = document.getElementById("color-select");
+
+const newColors = ["green", "yellow", "purple", "indigo", "orange", "brown"];
+
+newColors.forEach((color) => {
+  const option = document.createElement("option");
+  option.value = color;
+  option.textContent = color.charAt(0).toUpperCase() + color.slice(1);
+  colorSelect.appendChild(option);
+});
