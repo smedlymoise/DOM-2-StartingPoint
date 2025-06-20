1d2233fb1695e1352aec40c6e77d92865c227c15
Darrell Moreno <dmoreno1987@outlook.com>
2025-06-20 14:34:05 -0400

    Added feature in javascript file to click on single cell to change to chosen color

--- main.js
+++ main.js
@@ -5,3 +5,18 @@ root.addEventListener("click", (event) => {
   console.log(event.target.tagName);
   console.log(event.target);
 });
+
+let selectedColor = "red";
+
+const colorSelect = document.getElementById("color-select");
+
+colorSelect.addEventListener("change", (event) => {
+  selectedColor = event.target.value;
+  console.log("Selected color:", selectedColor);
+});
+
+root.addEventListener("click", (event) => {
+  if (event.target.tagName === "TD") {
+    event.target.style.backgroundColor = selectedColor;
+  }
+});
