06d974f76b302e8fa3df53f463ee4c348c5ad467
Webbney Vallon <webbvallon1999@gmail.com>
2025-06-22 10:58:33 -0400

    added the remove row story

--- main.js
+++ main.js
@@ -15,20 +15,6 @@ const removeColmnButton = document.getElementById('remove-column');
 
 
 
-
-// add row
-addRowButton.addEventListener('click' , () => {
-  const newRow = document.createElement('tr');
-  const columnCount = table.rows[0]?.cells.length || 0;
-  for(let i = 0; i < columnCount; i++){
-    const cell = document.createElement('td')
-    newRow.appendChild(cell);
-  }
-  tbody.appendChild(newRow);
-});
-
-
-
 // Remove Row (solution)
 removeRowButton.addEventListener("click", () => {
   const rows = tbody.rows;
@@ -38,35 +24,8 @@ removeRowButton.addEventListener("click", () => {
 });
 
 
-// add columns
-const addColumnButton = document.getElementById('add-column');
-addColumnButton.addEventListener('click', () => {
-  const thead = table.querySelector('thead');
-  if (thead) {
-    const headerRow = thead.rows[0];
-    const th = document.createElement('th');
-    th.textContent = `Column ${headerRow.cells.length + 1}`;
-    headerRow.appendChild(th);
-  }
-  Array.from(tbody.rows).forEach(row => {
-    const cell = document.createElement('th');
-    cell.textContent = '';
-    row.appendChild(cell);
-  });
-});
 
 
 
 
-// Remove columns (solution)
 
-removeColmnButton.addEventListener('click' , () => {
-  const columnCount = tbody.rows[0].cells.length;
- const allRows = table.querySelectorAll('tr');
- allRows.forEach(row => {
-    if (row.cells.length >= columnCount) {
-      row.deleteCell(columnCount - 1);
-
-    }
-  })
-});
