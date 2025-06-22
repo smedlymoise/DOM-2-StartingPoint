b1d1fc482541fa73f67a22c27a128bdaf0753026
Webbney Vallon <webbvallon1999@gmail.com>
2025-06-22 11:57:51 -0400

    Revert "Remove rows from grid"

--- main.js
+++ main.js
@@ -5,27 +5,3 @@ root.addEventListener("click", (event) => {
   console.log(event.target.tagName);
   console.log(event.target);
 });
-
-const  table = document.querySelector('table');
-const tbody = table.querySelector('tbody');
-const addRowButton = document.getElementById('add-row');
-const removeRowButton = document.getElementById('remove-row');
-const thead = table.querySelector('thead');
-const removeColmnButton = document.getElementById('remove-column');
-
-
-
-// Remove Row (solution)
-removeRowButton.addEventListener("click", () => {
-  const rows = tbody.rows;
-  if (rows.length > 0) {
-    tbody.deleteRow(rows - 1);
-  }
-});
-
-
-
-
-
-
-
