d67c2e251627c831f93587cb601ac1260149ebdb
Darrell Moreno <dmoreno1987@outlook.com>
2025-06-22 21:22:50 -0400

    Added feature to clear all colors from every cell

--- main.js
+++ main.js
@@ -1,31 +1,7 @@
-// Please feel free to change the JS as you see fit! This is just a starting point.
-
-const root = document.getElementById("root");
-root.addEventListener("click", (event) => {
-  console.log(event.target.tagName);
-  console.log(event.target);
+const clearGridButton = document.getElementById("clear-grid");
+clearGridButton.addEventListener("click", () => {
+    const squares = document.querySelectorAll("table td");
+    squares.forEach(cell =>{
+        cell.computedStyleMap.backgroundColor = "";
+    });
 });
-
-const  table = document.querySelector('table');
-const tbody = table.querySelector('tbody');
-const addRowButton = document.getElementById('add-row');
-const removeRowButton = document.getElementById('remove-row');
-const thead = table.querySelector('thead');
-const removeColmnButton = document.getElementById('remove-column');
-
-
-
-// Remove Row (solution)
-removeRowButton.addEventListener("click", () => {
-  const rows = tbody.rows;
-  if (rows.length > 0) {
-    tbody.deleteRow(rows - 1);
-  }
-});
-
-
-
-
-
-
-
